50d9c218f591e79324e96d4418cfc2fe1e7b9a74
Florin Pop <popflorin1705@yahoo.com>
2018-03-20 23:40:46 +0200

    finished project 5

---
+++ Projects/005 - Physics - Steering behaviors/README.md
@@ -0,0 +1,15 @@
+## Description
+Demonstrating different physics behaviors in p5js:
+- Seek
+- Flee
+- Align
+- Separate
+
+Inspired by Daniel Shiffman's [Youtube Videos](https://www.youtube.com/watch?v=JIz2L4tn5kM&list=PLRqwX-V7Uu6YHt0dtyf4uiw8tKOxQLvlW).
+
+## Link
+You can see the version live on [Codepen](https://codepen.io/FlorinPop17/full/WzGQxp).
+
+## Credits
+- Daniel Shiffman - [The Coding Train](https://www.youtube.com/channel/UCvjgXvBlbQiydffZU7m1_aw)
+- Craig Reynolds - [Steering Behaviors](https://www.red3d.com/cwr/steer/)
--- Projects/005 - Physics - Steering behaviors/sketch.js
+++ Projects/005 - Physics - Steering behaviors/sketch.js
@@ -1,8 +1,10 @@
 const VEHICLES = [];
-const SCREEN_SIZE = Math.min(600, window.innerWidth);
+const SCREEN_SIZE = Math.min(640, window.innerWidth);
+const SELECT = document.getElementById('steer');
+let selected_behavior = SELECT.value;
 
 function setup() {
-    createCanvas(SCREEN_SIZE, SCREEN_SIZE);
+    createCanvas(SCREEN_SIZE, SCREEN_SIZE * 3 / 4);
 
     for(let i = 0; i < 50; i++) {
         VEHICLES.push(new Vehicle(random(width), random(height)));
@@ -13,8 +15,7 @@ function draw() {
     background(200);
 
     for(let vehicle of VEHICLES) {
-        vehicle.align(VEHICLES);
-        vehicle.separate(VEHICLES);
+        applySelectedBehavior(selected_behavior, vehicle);
         vehicle.update();
         vehicle.checkEdges();
         vehicle.draw();
@@ -24,3 +25,30 @@ function draw() {
 function mousePressed() {
     VEHICLES.push(new Vehicle(mouseX, mouseY));
 }
+
+function applySelectedBehavior(behavior, vehicle) {
+    const mouse = createVector(mouseX, mouseY);
+
+    switch(behavior) {
+        case 'align': {
+            vehicle.align(VEHICLES);
+            break;
+        }
+        case 'separate': {
+            vehicle.separate(VEHICLES);
+            break;
+        }
+        case 'seek': {
+            vehicle.arrival(mouse);
+            break;
+        }
+        case 'flee': {
+            vehicle.flee(mouse);
+            break;
+        }
+    }
+}
+
+SELECT.addEventListener('change', (e) => {
+    selected_behavior = e.target.value;
+});
