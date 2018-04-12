039b7033f161380b62345e79f5ff239d2577c247
Florin Pop <popflorin1705@yahoo.com>
2018-04-12 21:17:33 +0300

    finished project 013 & added README + gif example

---
+++ Projects/013 - Rolling Ball in 3D Canvas/README.md
@@ -0,0 +1,11 @@
+![Rolling Ball in 3D Canvas](./example.gif)
+
+## Description
+Roll the ball in a canvas by moving your mouse over it.
+
+## Link
+You can see the version live on [Codepen](https://codepen.io/FlorinPop17/full/mxgJxX).
+
+## Credits
+- Built with [p5js](https://p5js.org)
+- Inspired from this [Codepen](https://codepen.io/winkerVSbecks/full/LdqVVL)
--- Projects/013 - Rolling Ball in 3D Canvas/script.js
+++ Projects/013 - Rolling Ball in 3D Canvas/script.js
@@ -1,23 +1,74 @@
 const STEP = 15;
 const BALL_SIZE = 75;
+const RANGE = 20;
+const SCREEN_SIZE = Math.min(600, window.innerWidth);
+const BALL_POSITION = {
+    x: SCREEN_SIZE / 2,
+    y: SCREEN_SIZE / 2
+};
+
+const BALL_VELOCITY = {
+    x: 0,
+    y: 0
+}
+
+let canvas;
+
+function transformCanvas() {
+    const x = map(mouseX, 0, SCREEN_SIZE, RANGE, -RANGE, true);
+    const y = map(mouseY, 0, SCREEN_SIZE, -RANGE, RANGE, true);
+
+    BALL_VELOCITY.x = x / 5;
+    BALL_VELOCITY.y = -y / 5;
+    canvas.style.transform = `rotateX(${y}deg) rotateY(${x}deg)`;
+}
+
+function resetCanvas() {
+    BALL_VELOCITY.x = 0;
+    BALL_VELOCITY.y = 0;
+    canvas.style.transform = `rotateX(0deg) rotateY(0deg)`;
+}
 
 function setup() {
-    createCanvas(600, 600);
+    canvas = createCanvas(SCREEN_SIZE, SCREEN_SIZE).canvas;
+    canvas.addEventListener('mousemove', transformCanvas)
+    canvas.addEventListener('mouseleave', resetCanvas)
 }
 
 function draw() {
-    background(255);
+    background(220);
     drawPoints();
     drawBall();
+    update();
+    checkBorders();
+}
+
+function update() {
+    BALL_POSITION.x += BALL_VELOCITY.x;
+    BALL_POSITION.y += BALL_VELOCITY.y;
+}
+
+function checkBorders() {
+    if(BALL_POSITION.x < BALL_SIZE) {
+        BALL_POSITION.x = BALL_SIZE;
+    } else if (BALL_POSITION.x > width - BALL_SIZE) {
+        BALL_POSITION.x = width - BALL_SIZE;
+    }
+
+    if(BALL_POSITION.y < BALL_SIZE) {
+        BALL_POSITION.y = BALL_SIZE;
+    } else if (BALL_POSITION.y > height - BALL_SIZE) {
+        BALL_POSITION.y = height - BALL_SIZE;
+    }
 }
 
 function drawPoints() {
     const points = width / STEP;
 
-    stroke(150);
+    stroke(175);
     strokeWeight(2);
-    for(let i = 0; i < width; i+= STEP) {
-        for(let j = 0; j < height; j+= STEP) {
+    for(let i = STEP; i < width; i+= STEP) {
+        for(let j = STEP; j < height; j+= STEP) {
             point(i, j);
         }
     }
@@ -25,6 +76,6 @@ function drawPoints() {
 
 function drawBall() {
     noStroke();
-    fill(0);
-    ellipse(width / 2, height / 2, BALL_SIZE * 2);
+    fill(100, 200);
+    ellipse(BALL_POSITION.x, BALL_POSITION.y, BALL_SIZE * 2);
 }
