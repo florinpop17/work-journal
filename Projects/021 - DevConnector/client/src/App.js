9c847433c4d7084e3e2356fefa64b7d3012ee212
Florin Pop <popflorin1705@yahoo.com>
2018-05-09 20:08:11 +0300

    added register page & event handlers

--- Projects/021 - DevConnector/client/src/App.js
+++ Projects/021 - DevConnector/client/src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Footer from "./components/layout/Footer";
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Footer from './components/layout/Footer';
 
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
-import "./App.css";
+import './App.css';
 
 class App extends Component {
     render() {
--- Projects/021 - DevConnector/client/src/components/auth/Register.js
+++ Projects/021 - DevConnector/client/src/components/auth/Register.js
@@ -1,10 +1,93 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            password2: '',
+            errors: {}
+        };
+    }
+
+    onChange = e => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        const { name, email, password, password2 } = this.state;
+        const newUser = { name, email, password, password2 };
+
+        console.log(newUser);
+    };
+
     render() {
         return (
-            <div>
-                <h1>Register</h1>
+            <div className="register">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Sign Up</h1>
+                            <p className="lead text-center">
+                                Create your DevConnector account
+                            </p>
+                            <form onSubmit={this.onSubmit}>
+                                <div className="form-group">
+                                    <input
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        placeholder="Name"
+                                        name="name"
+                                        value={this.state.name}
+                                        onChange={this.onChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="email"
+                                        className="form-control form-control-lg"
+                                        placeholder="Email Address"
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={this.onChange}
+                                    />
+                                    <small className="form-text text-muted">
+                                        This site uses Gravatar so if you want a
+                                        profile image, use a Gravatar email
+                                    </small>
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        className="form-control form-control-lg"
+                                        placeholder="Password"
+                                        name="password"
+                                        value={this.state.password}
+                                        onChange={this.onChange}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        className="form-control form-control-lg"
+                                        placeholder="Confirm Password"
+                                        name="password2"
+                                        value={this.state.password2}
+                                        onChange={this.onChange}
+                                    />
+                                </div>
+                                <input
+                                    type="submit"
+                                    className="btn btn-info btn-block mt-4"
+                                />
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
