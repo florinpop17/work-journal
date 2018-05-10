c243c9d57bc37d9e392e5cf9b6465d655e100fa1
Florin Pop <popflorin1705@yahoo.com>
2018-05-10 21:52:05 +0300

    CreateProfile component

--- Projects/021 - DevConnector/client/src/App.js
+++ Projects/021 - DevConnector/client/src/App.js
@@ -17,6 +17,8 @@ import Login from './components/auth/Login';
 
 import Dashboard from './components/dashboard/Dashboard';
 
+import CreateProfile from './components/create-profile/CreateProfile';
+
 import PrivateRoute from './components/common/PrivateRoute';
 
 import './App.css';
@@ -69,6 +71,13 @@ class App extends Component {
                                     exact
                                     path="/dashboard"
                                     component={Dashboard}
+                                />
+                            </Switch>
+                            <Switch>
+                                <PrivateRoute
+                                    exact
+                                    path="/create-profile"
+                                    component={CreateProfile}
                                 />
                             </Switch>
                         </div>
---
+++ Projects/021 - DevConnector/client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import TextFieldGroup from '../common/TextFieldGroup';
+
+class CreateProfile extends Component {
+    state = {
+        displaySocialInputs: false,
+        handle: '',
+        company: '',
+        website: '',
+        location: '',
+        status: '',
+        skills: '',
+        githubusername: '',
+        bio: '',
+        twitter: '',
+        facebook: '',
+        linkedin: '',
+        youtube: '',
+        instagram: '',
+        errors: {}
+    };
+
+    render() {
+        return (
+            <div className="create-profile">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">
+                                Create profile
+                            </h1>
+                            <p className="lead text-center">
+                                Let's get some information to make your profile
+                                stand out!
+                            </p>
+                            <small className="d-block pb-3">
+                                * = required fields
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+CreateProfile.propTypes = {
+    profile: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    profile: state.profile,
+    errors: state.errors
+});
+
+export default connect(null)(CreateProfile);
