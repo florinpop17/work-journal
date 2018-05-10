61767b706329834316484c12134b3610ff42838a
Florin Pop <popflorin1705@yahoo.com>
2018-05-10 06:08:37 +0300

    redux & reducers & redux devtools

--- Projects/021 - DevConnector/client/src/App.js
+++ Projects/021 - DevConnector/client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -13,17 +15,23 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div className="App">
-                    <Navbar />
-                    <Route exact path="/" component={Landing} />
-                    <div className="container">
-                        <Route exact path="/register" component={Register} />
-                        <Route exact path="/login" component={Login} />
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Navbar />
+                        <Route exact path="/" component={Landing} />
+                        <div className="container">
+                            <Route
+                                exact
+                                path="/register"
+                                component={Register}
+                            />
+                            <Route exact path="/login" component={Login} />
+                        </div>
+                        <Footer />
                     </div>
-                    <Footer />
-                </div>
-            </Router>
+                </Router>
+            </Provider>
         );
     }
 }
---
+++ Projects/021 - DevConnector/client/src/reducers/authReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+};
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
