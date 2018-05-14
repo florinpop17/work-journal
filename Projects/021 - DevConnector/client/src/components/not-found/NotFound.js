d57da3b1dc8d06a11d24fd654a53de669c1d0ed1
Florin Pop <popflorin1705@yahoo.com>
2018-05-14 08:32:47 +0300

    NotFound component

---
+++ Projects/021 - DevConnector/client/src/components/not-found/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            <h1>Page Not Found</h1>
+            <p>This page doesn't exist</p>
+        </div>
+    );
+};
