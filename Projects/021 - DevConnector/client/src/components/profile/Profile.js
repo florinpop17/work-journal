d91bd0652c33288a0495e985136c4789e8607367
Florin Pop <popflorin1705@yahoo.com>
2018-05-14 08:10:30 +0300

    ProfileAbout & ProfileCreds

--- Projects/021 - DevConnector/client/src/components/profile/Profile.js
+++ Projects/021 - DevConnector/client/src/components/profile/Profile.js
@@ -41,7 +41,10 @@ class Profile extends Component {
                     </div>
                     <ProfileHeader profile={profile} />
                     <ProfileAbout profile={profile} />
-                    <ProfileCreds />
+                    <ProfileCreds
+                        education={profile.education}
+                        experience={profile.experience}
+                    />
                     <ProfileGithub />
                 </div>
             );
--- Projects/021 - DevConnector/client/src/components/profile/ProfileAbout.js
+++ Projects/021 - DevConnector/client/src/components/profile/ProfileAbout.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import isEmpty from '../../validation/isEmpty';
 
 class ProfileAbout extends Component {
     render() {
+        const { profile } = this.props;
+
+        // First name
+        const firstName = profile.user.name.trim().split(' ')[0];
+
         return (
-            <div>
-                <h1>Profile About</h1>
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="card card-body bg-light mb-3">
+                        <h3 className="text-center text-info">
+                            {firstName}'s Bio
+                        </h3>
+                        <p className="lead">
+                            {isEmpty(profile.bio) ? (
+                                <span>
+                                    {firstName} does not have a bio yet.
+                                </span>
+                            ) : (
+                                <span>{profile.bio}</span>
+                            )}
+                        </p>
+                        <hr />
+                        <h3 className="text-center text-info">Skill Set</h3>
+                        <div className="row">
+                            <div className="d-flex flex-wrap justify-content-center align-items-center">
+                                {profile.skills.map((skill, index) => (
+                                    <div className="p-3" key={index}>
+                                        <i className="fa fa-check" /> {skill}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
+
+ProfileAbout.propTypes = {
+    profile: PropTypes.object.isRequired
+};
+
 export default ProfileAbout;
