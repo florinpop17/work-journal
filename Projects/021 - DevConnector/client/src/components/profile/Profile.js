480bf5ddf2287282badf4ef6722d7a2f573526ff
Florin Pop <popflorin1705@yahoo.com>
2018-05-14 07:57:38 +0300

    profileHeader

--- Projects/021 - DevConnector/client/src/components/profile/Profile.js
+++ Projects/021 - DevConnector/client/src/components/profile/Profile.js
@@ -21,12 +21,38 @@ class Profile extends Component {
     };
 
     render() {
+        const { profile, loading } = this.props.profile;
+        let profileContent;
+
+        if (profile === null || loading) {
+            profileContent = <Spinner />;
+        } else {
+            profileContent = (
+                <div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <Link
+                                to="/profiles"
+                                className="btn btn-light mb-3 float-left"
+                            >
+                                Back to profiles{' '}
+                            </Link>
+                        </div>
+                    </div>
+                    <ProfileHeader profile={profile} />
+                    <ProfileAbout profile={profile} />
+                    <ProfileCreds />
+                    <ProfileGithub />
+                </div>
+            );
+        }
         return (
-            <div>
-                <ProfileHeader />
-                <ProfileAbout />
-                <ProfileCreds />
-                <ProfileGithub />
+            <div className="profile">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">{profileContent}</div>
+                    </div>
+                </div>
             </div>
         );
     }
