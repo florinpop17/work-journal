217a7d686c400066dff18c8804c9107cbc31a87e
Florin Pop <popflorin1705@yahoo.com>
2018-05-15 06:52:37 +0300

    show Posts in Feed

--- Projects/021 - DevConnector/client/src/components/posts/PostItem.js
+++ Projects/021 - DevConnector/client/src/components/posts/PostItem.js
@@ -1,9 +1,67 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class PostItem extends Component {
+    onDeleteClick = id => {};
+
     render() {
-        return <div />;
+        const { post, auth } = this.props;
+        const { name, avatar, text, likes, user, _id } = post;
+
+        return (
+            <div className="card card-body mb-3">
+                <div className="row">
+                    <div className="col-md-2">
+                        <a href="profile.html">
+                            <img
+                                className="rounded-circle d-none d-md-block"
+                                src={avatar}
+                                alt={name}
+                            />
+                        </a>
+                        <br />
+                        <p className="text-center">{name}</p>
+                    </div>
+                    <div className="col-md-10">
+                        <p className="lead">{text}</p>
+                        <button type="button" className="btn btn-light mr-1">
+                            <i className="text-info fas fa-thumbs-up" />
+                            <span className="badge badge-light">
+                                {likes.length}
+                            </span>
+                        </button>
+                        <button type="button" className="btn btn-light mr-1">
+                            <i className="text-secondary fas fa-thumbs-down" />
+                        </button>
+                        <Link to={`/post/${_id}`} className="btn btn-info mr-1">
+                            Comments
+                        </Link>
+                        {user === auth.user.id ? (
+                            <button
+                                onClick={() => this.onDeleteClick(_id)}
+                                type="button"
+                                className="btn btn-danger mr-1"
+                            >
+                                <i className="fas fa-times" />
+                            </button>
+                        ) : null}
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
-export default PostItem;
+PostItem.propTypes = {
+    post: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps)(PostItem);
